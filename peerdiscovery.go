3e31e46a00cbf81981b67452223d09922b7ff24a
youngsheng <young@space.lan>
2019-11-21 22:32:47 +0800

    Fix!

--- peerdiscovery.go
+++ peerdiscovery.go
@@ -2,51 +2,12 @@ package peerdiscovery
 
 import (
 	"fmt"
-	flutter "github.com/go-flutter-desktop/go-flutter"
-	"github.com/go-flutter-desktop/go-flutter/plugin"
 	"github.com/schollz/peerdiscovery"
 	"log"
 	"time"
 )
 
-const channelName = "youngspace.xyz/go/peerdiscovery"
-
-type PeerDiscoveryPlugin struct{}
-
-var _ flutter.Plugin = &PeerDiscoveryPlugin{} // compile-time type check
-
-// InitPlugin creates a MethodChannel and set a HandleFunc to the
-// shared 'getBatteryLevel' method.
-// https://godoc.org/github.com/go-flutter-desktop/go-flutter/plugin#MethodChannel
-//
-// The plugin is using a MethodChannel through the StandardMethodCodec.
-//
-// You can also use the more basic BasicMessageChannel, which supports basic,
-// asynchronous message passing using a custom message codec.
-// You can also use the specialized BinaryCodec, StringCodec, and JSONMessageCodec
-// struct, or create your own codec.
-//
-// The JSONMessageCodec was deliberately left out because in Go its better to
-// decode directly to known structs.
-func (PeerDiscoveryPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
-	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
-	channel.HandleFunc("getBatteryLevel", handleGetBatteryLevel)
-	channel.HandleFunc("peerDicovers", peerDicovers)
-	return nil // no error
-}
-
-// handleGetBatteryLevel is called when the method getBatteryLevel is invoked by
-// the dart code.
-// The function returns a fake battery level, without raising an error.
-//
-// Supported return types of StandardMethodCodec codec are described in a table:
-// https://godoc.org/github.com/go-flutter-desktop/go-flutter/plugin#StandardMessageCodec
-func handleGetBatteryLevel(arguments interface{}) (reply interface{}, err error) {
-	batteryLevel := int32(55) // Your platform-specific API
-	return batteryLevel, nil
-}
-
-func peerDicovers(arguments interface{}) (reply interface{}, err error){
+func peerDicovers() (str string, err error){
 	discoveries, err := peerdiscovery.Discover(peerdiscovery.Settings{
 		Limit:     -1,
 		Payload:   []byte("ubuntu"),
@@ -74,6 +35,10 @@ func peerDicovers(arguments interface{}) (reply interface{}, err error){
 		}
 	}
 
+	ipAddressStr := ""
+	for _, ip := range ipAddress {
+		ipAddressStr += (ip + ",")
+	}
 
-	return ipAddress, nil
+	return ipAddressStr, nil
 }
